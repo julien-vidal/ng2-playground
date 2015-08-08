4a004809f34e124cd33b6f489c7602bd838007af
VIDAL Julien <julien.vidal.jv@gmail.com>
2015-08-08 14:06:52 +0200

    Fix git merge issues

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
-if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
     switch (arguments.length) {
         case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
@@ -7,27 +7,30 @@ if (typeof __decorate !== "function") __decorate = function (decorators, target,
         case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
     }
 };
-if (typeof __metadata !== "function") __metadata = function (k, v) {
+var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
 var pokemonDataService_1 = require('pokemonDataService');
+var localstorageService_1 = require('localstorageService');
 // Annotation section
 var MyAppComponent = (function () {
-    //constructor(pokemonDataService: PokemonDataService) {
-    function MyAppComponent() {
-        //this.pokemonDataService = pokemonDataService;
-        //this.pokemonDataService.getPokedex().subscribe(pokedex => console.log(pokedex));
+    function MyAppComponent(pokemonDataService) {
+        console.log("App : Constructor");
+        //console.dir(ls);
+        //
+        //  this.pokemonDataService = pokemonDataService;
+        //  this.pokemonDataService.getPokedex().subscribe(pokedex => console.log(pokedex));
     }
     MyAppComponent = __decorate([
         angular2_1.Component({
             selector: 'my-app',
-            appInjector: [pokemonDataService_1.PokemonDataService]
+            appInjector: [localstorageService_1.LocalstorageService, pokemonDataService_1.PokemonDataService]
         }),
         angular2_1.View({
             template: '<h1>Hello {{ name }}</h1>'
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [pokemonDataService_1.PokemonDataService])
     ], MyAppComponent);
     return MyAppComponent;
 })();
